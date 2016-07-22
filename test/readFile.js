8e8fbb99a7c5b374cfa4c2d938dcae2810c4e218
Tomasz Kalfas <tomasz.kalfas@schibsted.pl>
2016-07-22 13:10:45 +0200

    read file with mocked fs

--- test/readFile.js
+++ test/readFile.js
@@ -7,7 +7,7 @@ describe('Read file', () => {
   it('[integration-test] should read data from file', function *() {
     // given
     const fileName = __dirname + '/symbols';
-    const reader = require('../lib/reader');
+    const reader = require('../lib/reader')();
 
     // when
     const data = yield reader.read(fileName);
@@ -18,11 +18,47 @@ describe('Read file', () => {
   it('[integration-test] should throw an error if file doesnt exist', function *() {
     // given
     const fileName = __dirname + '/fileDoesNotExist';
-    const reader = require('../lib/reader');
+    const reader = require('../lib/reader')();
 
     // when
     try {
       yield reader.read(fileName);
+    } catch (e) {
+      // then
+      assert.ok(e);
+    }
+  });
+  it('should read file', function *() {
+    // given
+    const file = __dirname + '/symbols';
+    const mockedFs = {
+      readFile: (fileName, encoding, cb) => {
+        assert.equal(fileName, file);
+        assert.equal(encoding, 'utf8');
+        cb(null, 'data');
+      }
+    };
+    const reader = require('../lib/reader')({fs: mockedFs});
+
+    const data = yield reader.read(file)
+
+    assert.ok(data);
+  });
+  it('should throw error', function *() {
+    // given
+    const file = __dirname + '/fileDoesNotExist';
+    const mockedFs = {
+      readFile: (fileName, encoding, cb) => {
+        assert.equal(fileName, file);
+        assert.equal(encoding, 'utf8');
+        cb(new Error());
+      }
+    };
+    const reader = require('../lib/reader')({fs: mockedFs});
+
+    // when
+    try {
+      yield reader.read(file);
     } catch (e) {
       // then
       assert.ok(e);
